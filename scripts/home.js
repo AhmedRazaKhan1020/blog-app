356b1c76b03857212705bcb84691bdf6d3f4e9fa
AhmedRazaKhan1020 <cadetahmedraza@gmail.com>
2025-02-10 01:12:20 +0500

    ch2

---
+++ scripts/home.js
@@ -0,0 +1,175 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getFirestore, getDocs, doc,addDoc  ,collection,deleteDoc,getDoc,updateDoc} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js"; 
+// import {profileImage} from "./register.js";
+// Ensure you're using "firebase-firestore.js" for Firestore functions
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBTeqWiaL854KXqyXixioy1nuBjx_v18o0",
+    authDomain: "social-project-29ea5.firebaseapp.com",
+    projectId: "social-project-29ea5",
+    storageBucket: "social-project-29ea5.firebasestorage.app",
+    messagingSenderId: "929013255817",
+    appId: "1:929013255817:web:2e8f044c5d6f9dfd3e0058",
+    measurementId: "G-VDTVMGQV93"
+};
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+// console.log(db);
+
+const form = document.getElementById('userForm');
+let container = document.querySelector("#container")
+
+
+
+
+let userProfilePicUrl 
+
+let myWidget = cloudinary.createUploadWidget({
+    cloudName: 'diqpvofng',
+    uploadPreset: 'saylani'
+}, (error, result) => {
+    if (!error && result && result.event === "success") {
+        console.log('Done! Here is the image info: ', result.info);
+        userProfilePicUrl = result.info.secure_url
+    }
+}
+)
+
+document.getElementById("upload_widget").addEventListener("click", function (event) {
+    event.preventDefault()
+    myWidget.open();
+}, false);
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    let title = document.getElementById('title').value;
+  let description = document.getElementById('description').value;
+
+   // to empty input filed function
+
+    function toEmptyinputFiled(){
+       title = document.querySelector("#title").value = '';
+       description = document.querySelector("#description").value = '';
+   };
+       // to add data from user click 
+    try {
+        const docRef = await addDoc(collection(db, "users"), {
+         title:title,
+         description:description,
+         userProfilePicUrl:userProfilePicUrl
+        });
+        // console.log("Document written with ID: ", docRef.id);
+      
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+      renderData()
+      container.innerHTML = `<p>Data Added Suceessfully</p>`
+       toEmptyinputFiled()
+  });
+
+
+  // render data on screen 
+
+ async function renderData(){
+   
+    try {
+
+
+     const docRef = await getDocs(collection(db,'users'));
+     
+     
+
+      let html = ""
+      let table = document.querySelector("table");
+
+     docRef.forEach(element => {
+         const userData = element.data();
+         const docid = element.id; // jese he data database sa aye ga os ke aik id hoge jise docId kha ta hi
+        //  console.log(globalArray);
+          html+= `
+          <tr>
+          <td>${userData.description}</td>
+          <td><img src="${userData.userProfilePicUrl}" alt="img" class="image" style="width:100px;height:100px;"></td>
+           <td> <button id="delbtn" onclick="deletMethod('${docid}')">Delete</button></td>
+           <td> <button id="delbtn" onclick="UpdatedtMethod('${docid}')">UPDate</button></td>
+           </tr>
+           
+       `
+     });
+   
+     table.innerHTML = html
+      
+    } catch (error) {
+        console.log(error)
+    }
+     
+     
+}
+
+// delet data 
+window.deletMethod = async function(docid){
+  container.innerHTML = ''
+
+  const docRef = doc(db,'users',docid);
+  await deleteDoc(docRef)
+  renderData()
+  container.innerHTML = `<p>Data Delet Suceessfully.</p>`
+
+}
+
+// updated data from form 
+window.UpdatedtMethod =  async function(id){
+   try {
+    const docRef = await  getDoc(doc(db,'users',id))
+    const currentUser = docRef.data();
+
+    console.log(currentUser) // object is return from db is obj ma title or desction hai ;
+
+      document.querySelector("#title").value = currentUser.title;
+      document.querySelector("#description").value =currentUser.description;
+
+      let sumbitbtn = document.querySelector('.sumbit');
+       let btn = document.querySelector(".updatedbtn");
+
+       btn.classList.add('show');
+      sumbitbtn.classList.add('hide');
+
+      btn.addEventListener('click', async()=>{
+          const newtilte = document.querySelector("#title").value;
+          const newdescription = document.querySelector("#description").value;
+
+          if(newtilte !== null && newdescription !== null){
+              await updateDoc(doc(db,'users',id),{
+                   title:newtilte,
+                   description:newdescription
+              });
+              
+              renderData()
+      container.innerHTML = ` <p>Data Updated  Suceessfully/<p>`
+
+              sumbitbtn.classList.remove('hide');
+              btn.classList.remove('show');
+
+          }
+          
+      })
+
+   } catch (error) {
+     console.log(error)
+   }
+}
+
+
+renderData()
+
+// const promg=document.querySelector("#promg");
+// promg.innerHTML+=`
+//  <img  alt="img" src="${profileImage}" >
+// `
--- scripts/register.js
+++ scripts/register.js
@@ -1,11 +1,13 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 import { collection, addDoc } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 import { auth, db } from "./config.js";
-const form = document.querySelector("#form");
-const email = document.querySelector("#email");
-const name = document.querySelector("#name");
-const password = document.querySelector("#password");
-const p = document.querySelector("#error");
+
+const form = document.querySelector('#form');
+const email = document.querySelector('#registerEmail');
+const password = document.querySelector('#registerPassword');
+const fullName = document.querySelector('#fullName');
+
+
 
 
 let userProfilePicUrl = ""
@@ -21,47 +23,45 @@ let myWidget = cloudinary.createUploadWidget({
 }
 )
 
-document.getElementById("upload_widget").addEventListener("click", function(event) {
-    event.stopPropagation(); // Optional, depending on your needs
-    if (typeof myWidget !== 'undefined') {
-        myWidget.open();
-    } else {
-        console.error('myWidget is not defined or initialized.');
-    }
+document.getElementById("upload_widget").addEventListener("click", function (event) {
+    event.preventDefault()
+    myWidget.open();
 }, false);
 
 
-form.addEventListener("submit", event => {
-    event.preventDefault();
-    // p.innerHTML = ""
-    console.log(name.value);
-    console.log(email.value)
-    
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    console.log(email.value);
+    console.log(password.value);
+    console.log(fullName.value);
 
-    createUserWithEmailAndPassword(auth, email.value, password.value,name.value)
-    .then(async (userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-         window.location="./login.html"
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+        .then(async (userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+            window.location="./login.html"
 
-        try {
-            const docRef = await addDoc(collection(db, "users"), {
-                fullName: name.value,
-                email: email.value,
-                // profileImage: userProfilePicUrl,
-                uid: user.uid
-            });
-            console.log("Document written with ID: ", docRef.id);
-        } catch (e) {
-            console.error("Error adding document: ", e);
-        }
+            try {
+                const docRef = await addDoc(collection(db, "users"), {
+                    fullName: fullName.value,
+                    email: email.value,
+                    profileImage: userProfilePicUrl,
+                    uid: user.uid
+                });
+                console.log("Document written with ID: ", docRef.id);
+            } catch (e) {
+                console.error("Error adding document: ", e);
+            }
+            // export const profileImage= profileImage;
 
 
-    })
+        })
+        .catch((error) => {
+            const errorMessage = error.message;
+            console.log(errorMessage);
+            p.innerHTML = errorMessage
+        });
+
+
 })
-        // .catch((error) => {
-        //     const errorMessage = error.message;
-        //     console.log(errorMessage);
-        //     p.innerHTML = errorMessage
-        // });
-
+export const profileImage= profileImage;
